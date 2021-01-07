2f846615bf918ba44d546b10c907000bf4f1842f
YiChing <bgtuyut0505@gmail.com>
2021-01-07 12:20:38 +0800

    11/7 final project

---
+++ FoodPin/weather/weathermodel.swift
@@ -0,0 +1,46 @@
+//
+//  weathermodel.swift
+//  FoodPin
+//
+//  Created by NDHU_CSIE on 2021/1/7.
+//  Copyright © 2021 NDHU_CSIE. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel: Decodable {
+ 
+  let localObservationDateTime: String
+  let epochTime: TimeInterval
+  let weatherText: String
+  let isDayTime: Bool
+ 
+  struct Temperature: Decodable {
+      struct Metric: Decodable {
+          let value: Double
+          let unit: String
+          let unitType: Int
+          private enum CodingKeys: String, CodingKey {
+              case value = "Value"
+              case unit = "Unit"
+              case unitType = "UnitType"
+          }
+      }
+      let metric: Metric
+      private enum CodingKeys: String, CodingKey {
+          case metric = "Metric"
+      }
+  }
+  let temperature: Temperature
+  let mobileLink: URL
+  let link: URL
+  private enum CodingKeys: String, CodingKey {
+      case localObservationDateTime = "LocalObservationDateTime"
+      case epochTime = "EpochTime"
+      case weatherText = "WeatherText"
+      case isDayTime = "IsDayTime"
+      case temperature = "Temperature"
+      case mobileLink = "MobileLink"
+      case link = "Link"
+  }
+}
